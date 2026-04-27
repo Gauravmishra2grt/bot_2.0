1cc2bc45e918e879bc932745a2a4e494ffd6475f
Gaurav Mishra <mgaurav2dgrt@gmail.com>
2026-04-27 22:10:52 +0530

    fix: convert api to vercel serverless handler

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,9 @@
-import app from "../src/app.js";
+import { handleChat } from "../src/controllers/chatController.js";
 
-export default app;
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  return handleChat(req, res);
+}
