3a99a5ae6387761ea2ef58582f09a5a999bfe551
Gaurav Mishra <mgaurav2dgrt@gmail.com>
2026-04-27 22:04:13 +0530

    setup backend for vercel deployment

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -2,7 +2,9 @@ import express from "express";
 import { handleChat } from "../controllers/chatController.js";
 
 const router = express.Router();
-
+router.get("/", (req, res) => {
+  res.send("API is working 🚀");
+});
 router.post("/", handleChat);
 
 export default router;
